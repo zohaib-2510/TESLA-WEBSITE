8600b968a745fd75baa970ee61f02c65e1730198
zohaib-2510 <140811807+zohaib-2510@users.noreply.github.com>
2023-10-22 02:34:14 -0700

    complete

--- app.js
+++ app.js
@@ -1,11 +1,42 @@
-var heart = document.getElementById("heart");
-var btn = document.querySelector("btnlike");
-
+    var btn = document.getElementsByClassName("btnlike1");
+var heart=document.getElementById("heart")
 function myLike(){
-    btn.addEventListener("click",function(){
-        heart.style.backgroundColor="red"
-        console.log("ok");
-    })
+    heart.style.color="red"
 }
-myLike()
-console.log("ljnoihk");
+var heart1 = document.getElementById("heart1")
+var heart2 = document.getElementById("heart2")
+var heart3 = document.getElementById("heart3")
+var heart4 = document.getElementById("heart4")
+var heart5 = document.getElementById("heart5")
+
+
+
+
+
+function myLike1(){
+    heart1.style.color="red"
+}
+function myLike2(){
+    heart2.style.color="red"
+}
+function myLike3(){
+    heart3.style.color="red"
+}
+function myLike4(){
+    heart4.style.color="red"
+}
+function myLike5(){
+    heart5.style.color="red"
+}
+
+
+
+var rp=document.getElementById("read");
+function readmoree(){
+    rp.innerText="obcaecati error ullam ad veritatis est natus deleniti"
+    
+    var readMore = document.getElementById("btn1")
+    readMore.style.display="none"
+    
+console.log(readMore)
+}
